386b43185e33be3d4760c4adda41a109343387e1
Freelancer-Rafel <refelresel@gmail.com>
2023-04-06 00:09:32 +0600

    my final work

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    console.log(cart);
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+    }
+
+    let shipping = 12.99;
+    if (total > 35) {
+        shipping = 0;
+    } 
+    else if (total > 15) {
+        shipping = 4.99;
+    }
+    else if (total > 0) {
+        shipping = 12.99;
+    }
+
+    const tex = total / 10;
+    const grandTotal = (total + shipping + tex);
+
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
+
+    return (
+        <div>
+            <h4>Order Summary: </h4>
+            <p>Items Order: {cart.length}</p>
+            <p>Product Price: {formatNumber(total)}</p>
+            <p><small>Shipping Cost: {formatNumber(shipping)}</small></p>
+            <p><small>Tex + Vat: {formatNumber(tex)}</small></p>
+            <p>Total Price: {formatNumber(grandTotal)}</p>
+        </div>
+    );
+};
+
+export default Cart;
